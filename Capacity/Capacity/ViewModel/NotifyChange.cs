cf4d4f027967260782e5749acea5732e97b432f0
arada <arada@perfilestecnologicos.com>
2020-04-07 21:05:49 -0600

    Add notifychange

---
+++ Capacity/Capacity/ViewModel/NotifyChange.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Capacity.ViewModel
+{
+    public abstract class NotifyChange : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
